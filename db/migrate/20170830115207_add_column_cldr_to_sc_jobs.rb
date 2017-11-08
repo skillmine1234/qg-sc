20aa295152ac7787e1055a3c325d26394a70ee1e
divyajayan <divya.jayan@quantiguous.com>
2017-11-08 12:35:32 +0530

    SY-282 new columns in sc_jobs

---
+++ db/migrate/20170830115207_add_column_cldr_to_sc_jobs.rb
@@ -0,0 +1,10 @@
+class AddColumnCldrToScJobs < ActiveRecord::Migration
+  def change
+ 	  add_column :sc_jobs, :cldr, :string, null: false, default: 'GREG', comment: 'the calendar which needs to be followed to run the job'   
+ 	  add_column :sc_jobs, :status_code, :string, :limit => 50, comment: 'the status of the job'   
+ 	  add_column :sc_jobs, :fault_code, :string, :limit => 50, comment: "the code that identifies the business failure reason/exception"
+ 	  add_column :sc_jobs, :fault_subcode, :string, :limit => 50, comment: "the error code that the third party will return"  
+ 	  add_column :sc_jobs, :fault_reason, :string, :limit => 1000, comment: "the english reason of the business failure reason/exception"  
+ 	  add_column :sc_jobs, :fault_bitstream, :text, comment: "the fault bitstream"                    
+  end
+end
--- spec/searchers/sc_job_searcher_spec.rb
+++ spec/searchers/sc_job_searcher_spec.rb
@@ -7,6 +7,10 @@ describe ScJobSearcher do
       sc_job = Factory(:sc_job, code: 'C123')
       ScJobSearcher.new({code: 'C123'}).paginate.should == [sc_job]
       ScJobSearcher.new({code: 'D234'}).paginate.should == []
+      
+      sc_job = Factory(:sc_job, status_code: 'COMPLETED')
+      ScJobSearcher.new({status_code: 'COMPLETED'}).paginate.should == [sc_job]
+      ScJobSearcher.new({status_code: 'FAILED'}).paginate.should == []
     end
   end
 end
