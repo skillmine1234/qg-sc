01e6845888a60d3a5d657ae7bfa990db23c471c3
divyajayan <divya.jayan@quantiguous.com>
2017-12-01 11:58:42 +0530

    SY-305 fixed seed file

--- db/fixtures/sc_proxy.rb
+++ db/fixtures/sc_proxy.rb
@@ -1,5 +1,7 @@
 ScProxy.seed(:url) do |s|
-  s.url = 'http://example.com'
+  s.url = 'https://wsh.yesbank.in:8082'
+  s.username = 'SCG30117'
+  s.password = 'Yesbank1'
   s.approval_status = 'A'
   s.created_by = 'Q'
 end
