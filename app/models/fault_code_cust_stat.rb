b0e9a019b07fd02bcc2ed4f5469c8038825f53d1
pravinkarale <pravinkarale21@gmail.com>
2023-07-14 13:06:09 +0530

    added

---
+++ app/models/fault_code_cust_stat.rb
@@ -0,0 +1,3 @@
+class FaultCodeCustStat < ActiveRecord::Base
+	self.table_name = 'console.fault_code_cust_stats'
+end
--- lib/qg/sc/version.rb
+++ lib/qg/sc/version.rb
@@ -1,5 +1,5 @@
 module Qg
   module Sc
-    VERSION = "3.6.0"
+    VERSION = "3.7.0"
   end
 end
