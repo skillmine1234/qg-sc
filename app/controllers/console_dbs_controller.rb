3cda2a4f3497f2e3ab214c7cc092d754c9fb84c7
pravinkarale <pravinkarale21@gmail.com>
2023-08-09 19:50:40 +0530

    added pagination

--- app/controllers/console_dbs_controller.rb
+++ app/controllers/console_dbs_controller.rb
@@ -1,15 +1,19 @@
 class ConsoleDbsController < ApplicationController
   # authorize_resource
+  require 'will_paginate/array'
   before_filter :authenticate_user!
   before_filter :block_inactive_user!
   respond_to :json
 
   def fault_code_master
     @fault_code_master = FaultCodeMaster.all
+    @fault_code_master = @fault_code_master.paginate(:per_page => 1, :page => params[:page]) rescue []
+
   end
 
   def fault_code_cust_stats
     @fault_code_stats = FaultCodeCustStat.all
+    @fault_code_stats = @fault_code_stats.paginate(:per_page => 1, :page => params[:page]) rescue []
   end
 
 end
--- lib/qg/sc/version.rb
+++ lib/qg/sc/version.rb
@@ -1,5 +1,5 @@
 module Qg
   module Sc
-    VERSION = "3.7.0"
+    VERSION = "3.8.0"
   end
 end
